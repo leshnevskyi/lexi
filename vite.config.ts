cdaf343711eff7e72ad52d751ff74e27eb78ef92
Leshnevskyi <leshnevskyi@gmail.com>
2022-10-11 01:54:56 +0300

    build: enable absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import {crx} from '@crxjs/vite-plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 import manifest from './manifest.json';
 
 export default defineConfig({
 	plugins: [
 		react(),
+		tsconfigPaths(),
 		crx({manifest}),
 	],
 });
