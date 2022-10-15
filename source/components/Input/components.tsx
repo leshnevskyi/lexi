eb18c2a962b8af6398dc7ebb43fcd659c0653714
Danylo-tk <danylo.tkach@gmail.com>
2022-10-15 12:41:55 +0300

    ui: add header component

---
+++ source/components/Input/components.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const WordInput = styled.input`
+    padding-left: 1.5rem;
+    width: 100%;
+    height: 5rem;
+    font-size: 2rem;
+    border: none;
+
+    &::placeholder{
+        color: #CFCFCF;
+    }
+`;
+
+export {WordInput};
---
+++ source/components/Selector/index.tsx
@@ -0,0 +1,11 @@
+import {Container} from './components';
+
+const Selector = () => {
+	return (
+		<Container>
+			<h1>ur mama</h1>
+		</Container>
+	);
+};
+
+export default Selector;
