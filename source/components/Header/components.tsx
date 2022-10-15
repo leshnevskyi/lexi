55c787cdb0663add6241c6d45459a1bdcf425fc1
Danylo-tk <danylo.tkach@gmail.com>
2022-10-15 12:41:55 +0300

    ui: add header component

---
+++ source/components/Header/components.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Container = styled.header`
+  height: 4rem;
+  padding: 0 1.5rem;
+  position: relative;
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #2c2c2c;
+  box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.05);
+`;
+
+export {Container};
---
+++ source/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import {Container} from './components';
+
+const Header = () => {
+	return (
+		<>
+			<Container>
+				<h1>lexi</h1>
+				<h1>Language</h1>
+				<h1>Switch</h1>
+			</Container>
+		</>
+	);
+};
+
+export default Header;
