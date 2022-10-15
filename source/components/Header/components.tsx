a201f828786b30b17e9b26032213383f07c4a4f5
Danylo-tk <danylo.tkach@gmail.com>
2022-10-15 13:28:00 +0300

    refactor: header component styles

--- source/components/Header/components.tsx
+++ source/components/Header/components.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
+import palette from 'style/palette';
+
 const Container = styled.header`
   height: 4rem;
   padding: 0 1.5rem;
   position: relative;
-  z-index: 999;
+  z-index: 1;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  color: #2c2c2c;
+  color: ${palette.ghostgrey};
   box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.05);
 `;
 
